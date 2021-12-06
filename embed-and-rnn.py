7b5d51986dcc69832ede7679d5de863f8452fe62
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2021-12-05 23:15:52 -0600

    more clarity

--- embed-and-rnn.py
+++ embed-and-rnn.py
@@ -7,16 +7,38 @@ import tensorflow.keras as keras
 import numpy as np
 from tensorflow.python.keras.layers.recurrent_v2 import GRU
 
-x1 = np.array([0, 1, 1])
-x2 = np.array([[0,1,1],[1,2,0]])
+# setup
+x1 = np.array([0, 1, 1]) # (T) = (3)
+x2 = np.array([[0,1,1],[1,2,0]]) # (N, T) = (2, 3)
 embed = keras.layers.Embedding(4,5)
 gru1 = keras.layers.GRU(15, return_sequences=True, return_state=True, )
+gru2 = keras.layers.GRU(15, return_sequences=True, return_state=False, )
+gru3 = keras.layers.GRU(15, return_sequences=False, return_state=False )
 dense1 = keras.layers.Dense(26)
 dense2 = keras.layers.Dense(26)
 
-z1 = embed(x1) # works even for one sample (T)
-z2 = embed(x2) # as well as multiple samples (N, T)
+"""
+FIRST OF ALL return_state, what does it do?
 
+so from my testing, a default GRU layer (gru3, in this case) will use the
+final hidden state as output. so (2, 15)
+
+if you set ONLY return_state to True, GRU will output a tuple of structure
+(normal_output, final_state), but it seems these two should be the same!
+and from my numpy math indeed, final_state==normal_output
+
+if you set ONLY return_sequences to True, GRU output because the hidden states at
+ALL timesteps. so (2, 3, 15)
+
+if you set BOTH return_sequences and return_state to True, you get a tuple of
+structure (sequences, final_state) which are each explained above
+"""
+
+# embed layer functionality testing
+z1 = embed(x1) # works even for one sample (T) -> (T, D)
+z2 = embed(x2) # as well as multiple samples (N, T) -> (N, T, D)
+
+# GRU func testing
 try:
     b = gru1(z1)
 except ValueError:
@@ -24,8 +46,8 @@ except ValueError:
 a = gru1(z2) # works for 3dim input
 
 # GRU output will be
-# 3dim -> if return_sequences is true
-# 2dim -> if only returning the last hidden state
+# 3dim (2, 3, 15) -> if return_sequences is true
+# 2dim (2, 15) -> if only returning the last hidden state
 # the last time step's output should be equiv to the last hiden state (default return of RNNs)
 test1 = (a[0][:, -1, :] - a[1] < 0.001)
 print(test1)
